fb2bb730ac8e9740512bfa4c9fe4e1949bec80d1
Наталья Арлазарова <sic15@yandex.ru>
2023-05-28 23:59:58 +0300

    Добавлены зависимости и докстринги

--- fish.py
+++ fish.py
@@ -9,10 +9,11 @@ load_dotenv()
 
 updater = Updater(token=os.getenv('TOKEN'))
 URL_TASK = os.getenv('URL_TASK')
-write_answer = ''
-try_number = ''
+write_answer = None
+try_number = None
 
 def new_task(update, context):
+    """Получение новой задачи от API."""
     global write_answer, try_number
     chat = update.effective_chat
     response=requests.get(URL_TASK).json()
@@ -22,9 +23,11 @@ def new_task(update, context):
     try_number = 1
 
 def answer():
+    """Чтение ответа пользователя."""
     updater.dispatcher.add_handler(MessageHandler(Filters.text, check_answer))
     
 def check_answer(update, context):
+    """Обработка ответа пользователя."""
     global try_number
     chat = update.effective_chat
     if write_answer == update.message.text:
@@ -38,10 +41,12 @@ def check_answer(update, context):
             context.bot.send_message(chat.id, text=f"Верный ответ: {write_answer}")
 
 def fish(update, context):
+    """Работа с рыбками."""
     chat = update.effective_chat
     context.bot.send_message(chat_id=chat.id, text='Рыбок пока не подключили')
 
 def wake_up(update, context):
+    """Запуск бота."""
     chat = update.effective_chat
     name = update.message.chat.first_name
     button = ReplyKeyboardMarkup([['/Math', '/Fish']], resize_keyboard=True)
