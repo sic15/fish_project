0e9534869bef54be88c8def2c1f3270633c5031a
Sergei Baryshevskii <black3knight@gmail.com>
2023-06-08 17:34:03 +0300

    Added decorator to init player if any handler call func + minor fixes

--- pull_req_fish.py
+++ pull_req_fish.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-updater = Updater(token=os.getenv('TOKEN'))
+updater: Updater = Updater(token=os.getenv('TOKEN'))
 URL_CAT = os.getenv('URL_CAT')
 URL = os.getenv('BKND_URL')
 button = ReplyKeyboardMarkup(
@@ -24,7 +24,7 @@ class PlayerSession():
 
     def get_or_create_player(self):
         search_response = requests.get(f'{URL}/player/?player_id={self.player_id}').json()
-        if len(search_response) == 0: # if not search_response
+        if not search_response: # if not search_response
             response = requests.post(f'{URL}/player/', json={"player_id": self.player_id})
             return response.json().get('id')
         return search_response[0].get('id')
@@ -36,6 +36,31 @@ class PlayerSession():
     # def destruct_current_task(self): # Проба пера
     #     self.current_task.delete()
 
+def wake_up(update, context):
+    """Запуск бота."""
+    player_init(update, context)
+    continue_work(update, context)
+
+def player_init(update, context):
+    global player_session
+    player_session = PlayerSession(update, context)
+
+def check_session(func):
+    def wrapper(*args, **kwargs):
+        if 'player_session' not in globals():
+            update, context = *args,
+            player_init(update, context)
+        func(*args, **kwargs)
+    return wrapper
+
+@check_session
+def continue_work(update, context):
+    score_message = f'Твой текущий счет: {player_session.get_user_score()}.'
+    context.bot.send_message(
+        chat_id=player_session.player_id,
+        text=f'Привет, {player_session.name}. {score_message} С чего начнём?',
+        reply_markup=button
+    )
 
 def get_new_image():
     """Получение фото котика."""
@@ -43,19 +68,23 @@ def get_new_image():
     random_cat = response[0].get('url')
     return random_cat
 
+@check_session
 def new_cat(update, context):
     """Отправка фото котика."""
     chat = update.effective_chat
-    context.bot.send_photo(chat.id, get_new_image())
+    context.bot.send_photo(player_session.player_id, get_new_image())
 
+@check_session
 def math(update, context):
     """Задачи по математике"""
     category = 1
     new_task(update, context, category)
 
+@check_session
 def fish(update, context):
     """Работа с рыбками."""
     category = 2
+    context.bot.send_message(player_session.player_id, 'В этой категории ответы надо писать на английском языке')
     new_task(update, context, category)
 
 def new_task(update, context, category):
@@ -65,10 +94,10 @@ def new_task(update, context, category):
     if current_task:
         player_session.current_task = current_task[0]
         player_session.try_number = 1
-        if player_session.current_task.get('text'):
-            context.bot.send_message(player_session.player_id, text=player_session.current_task.get('text'))
         if player_session.current_task.get('image'):
             context.bot.send_photo(player_session.player_id, player_session.current_task.get('image'))
+        if player_session.current_task.get('text'):
+            context.bot.send_message(player_session.player_id, text=player_session.current_task.get('text'))
         updater.dispatcher.add_handler(MessageHandler(Filters.text, check_answer))
     else: # обработать блок
         player_session.current_task = {'category': category} # пишем категорию при пустой задаче
@@ -81,11 +110,13 @@ def new_task(update, context, category):
         Filters.regex(r'баллы'),
         current_score))
 
+@check_session
 def current_score(update, context):
     """Проверка текущего счета."""
     score = player_session.get_user_score()
     context.bot.send_message(player_session.player_id, text=f"Ваш текущий счет {score}")
 
+@check_session
 def clean_category(update, context):
     """Очистка решенных задач пользователя по категории."""
     global player_session
@@ -125,19 +156,8 @@ def processing_score(context):
                    })
     context.bot.send_message(player_session.player_id, text=f'Баллы за эту задачу: {task_score}')
 
-def wake_up(update, context):
-    """Запуск бота."""
-    global player_session
-    player_session = PlayerSession(update, context)
-    continue_work(update, context)
 
-def continue_work(update, context):
-    score_message = f'Твой текущий счет: {player_session.get_user_score()}.'
-    context.bot.send_message(
-        chat_id=player_session.player_id,
-        text=f'Привет, {player_session.name}. {score_message} С чего начнём?',
-        reply_markup=button
-    )
+
 
 updater.dispatcher.add_handler(CommandHandler('start', wake_up))
 updater.dispatcher.add_handler(
